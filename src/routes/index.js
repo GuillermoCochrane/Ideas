5d0512596beeea9e42e27e8602cc87b31a8327af
GuillermoCochrane <guilleac81@gmail.com>
2024-03-26 18:03:47 -0300

    Agregado proyecto 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ideas' });
 });
 
+router.get('/1', function(req, res, next) {
+  res.render('1');
+});
+
 module.exports = router;
