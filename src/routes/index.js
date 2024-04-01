8fefaaa1fa8e37b80cd902b8778f7b100a3008a4
GuillermoCochrane <guilleac81@gmail.com>
2024-04-01 04:35:36 -0300

    Agregada ruta parametrizada de sliders

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ideas' });
 });
 
+
 router.get('/1', function(req, res, next) {
-  res.render('1');
+  res.render(url);
+});
+
+router.get('/sliders/:id', function(req, res, next) {
+  let url =`s${req.params.id}`;
+  res.render(url);
 });
 
 module.exports = router;
