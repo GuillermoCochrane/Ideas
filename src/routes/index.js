a397410aa662cc9e95cfdf70d9cab642f0e63a15
GuillermoCochrane <guilleac81@gmail.com>
2024-04-08 19:06:35 -0300

    Agregado ejemplo de details / Summary

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ideas' });
 });
 
-
 router.get('/:id', function(req, res, next) {
   res.render(req.params.id);
 });
 
 router.get('/sliders/:id', function(req, res, next) {
   let url =`s${req.params.id}`;
+  res.render(url);
+});
+
+router.get('/details/:id', function(req, res, next) {
+  let url =`d${req.params.id}`;
   res.render(url);
 });
 
