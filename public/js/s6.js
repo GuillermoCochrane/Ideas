7c6c54f6746f62bb28562efe4387219805cbfde9
GuillermoCochrane <guilleac81@gmail.com>
2024-05-11 22:05:20 -0300

    cambios en swiper

--- public/js/s6.js
+++ public/js/s6.js
@@ -1,6 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      }
+    },
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
